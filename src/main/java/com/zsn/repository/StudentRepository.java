412ce7cdfdb955cca4899300a95f358b6db43e87
zsn <1765509297@qq.com>
2020-08-13 20:22:52 +0800

    2020/8/13的项目

--- src/main/java/com/zsn/repository/StudentRepository.java
+++ src/main/java/com/zsn/repository/StudentRepository.java
@@ -1,9 +1,32 @@
 package com.zsn.repository;
 
 import com.zsn.pojo.Student;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+    List<Student> findByStudentName(String studentName);
+
+    List<Student> findByStudentNameLike(String studentName);
+
+    List<Student> findTop2ByStudentNameLike(String studentName);
+
+    //    @Query(value = "select s from Student s where s.studentName = ?1 and s.studentCard.cardId = ?2")
+//    @Query(value = "select s from Student s where s.studentName = :studentName " +
+//            "and s.studentCard.cardId = :cardId")
+    @Query(nativeQuery = true,
+            value = "select * from h_student where student_name = :studentName " +
+                    "and card_id = :cardId")
+    List<Student> getStudentsByParams(@Param("studentName") String studentName,
+                                      @Param("cardId") int cardId);
+
+
+
 }
--- src/main/java/com/zsn/service/StudentService.java
+++ src/main/java/com/zsn/service/StudentService.java
@@ -1,8 +1,19 @@
 package com.zsn.service;
 
+
 import com.zsn.pojo.Result;
+import com.zsn.pojo.SearchVo;
 import com.zsn.pojo.Student;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface StudentService {
-    public Result<Student> insertStudent(Student student);
+    Result<Student> insertStudent(Student student);
+
+    Student getStudentByStudentId(int studentId);
+
+    Page<Student> getStudentsBySearchVo(SearchVo searchVo);
+
+    List<Student> findByStudentName(String studentName,int cardId);
 }
